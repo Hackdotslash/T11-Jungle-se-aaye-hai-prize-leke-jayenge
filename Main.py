29a096e8adeb918edec2294c6232642871cc5e09
Sidhant Agarwal <sidhant11136@gmail.com>
2021-01-31 13:39:27 +0530

    Update Main.py
    Created Demo GUI and added demo components

--- Main.py
+++ Main.py
@@ -6,6 +6,8 @@ import pyaudio
 import wave
 import tkinter as tk
 from PIL import Image, ImageTk
+import tkinter.font as tkfont
+
 
 #INITIALISE HERE
 cap = cv2.VideoCapture(0)
@@ -13,10 +15,70 @@ aud = pyaudio.PyAudio()
 vid_frame_width = int(cap.get(3))
 vid_frame_height = int(cap.get(4))
 root = tk.Tk()
+root.minsize(1200,800)
 app = tk.Frame(root, bg="white")
-app.grid()
-lmain = tk.Label(app)
-lmain.grid()
+app.place (x = 870, y = 10)
+app2 = tk.Frame(root, bg="white")
+app2.place(x = 10, y = 10)
+app3 = tk.Frame(root, bg="white")
+app3.place(x = 10, y = 130)
+app4 = tk.Frame(root, bg="white")
+app4.place(x = 870, y =400)
+
+question_Font = tkfont.Font(family="Comic Sans MS",size=20 )
+option_Font = tkfont.Font(family="Comic Sans MS",size=15 )
+
+#DEMO FUNCTIONS..
+def selected():
+    print("The option selected is "+ str(var.get())) 
+
+def button_1():
+    print("You clicked button 1")
+def button_2():
+    print("You clicked button 2")
+def button_3():
+    print("You clicked button 3")
+def button_4():
+    print("You clicked button 4")
+def button_5():
+    print("You clicked button 5")
+def button_6():
+    print("You clicked button 6")
+
+
+#DEMO COMPONENTS
+question = tk.Label(app2, text = "The question will appear here! This is a sample question.\n 11. How many days are present in a leap year?",width = 50, padx = 10 , pady = 10, anchor = 'nw', font = question_Font)
+question.grid()
+lmain = tk.Label(app,anchor = 'ne')
+lmain.grid(row = 1, column = 1)
+var = tk.IntVar()
+op1 = tk.Radiobutton(app3, text="Option 1: 364", variable=var, value=1,command=selected, font = option_Font)
+op1.pack(anchor = 'w')
+op2 = tk.Radiobutton(app3, text="Option 2: 365", variable=var, value=2,command=selected, font = option_Font)
+op2.pack(anchor = 'w')
+op3 = tk.Radiobutton(app3, text="Option 3: 366", variable=var, value=3,command=selected, font = option_Font)
+op3.pack(anchor = 'w')
+op4 = tk.Radiobutton(app3, text="Option 4: 367", variable=var, value=4,command=selected, font = option_Font)
+op4.pack(anchor = 'w')
+
+b1 = tk.Button(app4, padx = 3,pady = 3, width = 10, height = 3, text = 'Q.1', command = button_1, relief = 'raised', bd = 4)
+b1.grid(column = 1, row = 1)
+b2 = tk.Button(app4, padx = 3,pady = 3, width = 10, height = 3, text = 'Q.2', command = button_2, relief = 'raised', bd = 4)
+b2.grid(column = 2, row = 1)
+b3 = tk.Button(app4, padx = 3,pady = 3, width = 10, height = 3, text = 'Q.3', command = button_3, relief = 'raised', bd = 4)
+b3.grid(column = 3, row = 1)
+b4 = tk.Button(app4, padx = 3,pady = 3, width = 10, height = 3, text = 'Q.4', command = button_4, relief = 'raised', bd = 4)
+b4.grid(column = 1, row = 2)
+b5 = tk.Button(app4, padx = 3,pady = 3, width = 10, height = 3, text = 'Q.5', command = button_5, relief = 'raised', bd = 4)
+b5.grid(column = 2, row = 2)
+b6 = tk.Button(app4, padx = 3,pady = 3, width = 10, height = 3, text = 'Q.6', command = button_6, relief = 'raised', bd = 4)
+b6.grid(column = 3, row = 2)
+
+
+#DYNAMIC COMPONENTS..
+status = tk.Label(app, text = 'The status will show here!', font = option_Font)
+status.grid(row = 2, column = 1)
+
 #DEFINE CONSTANTS HERE..
 SUSPICIOUS_THRESHOLD = 0.5
 CHUNK = 1024
@@ -57,11 +119,11 @@ def record_if_suspicious(vid_frame,aud_frame):
             video_output.write(vid_frame)
             audio_output.append(aud_frame)
             
-            final_audio = ("Audio_"+str(av_index),wb)
+            final_audio = ("Audio_"+str(av_index)+'.wav','wb')
             final_audio.setnchannels(CHANNELS)
-            final_audio.setsampwidth(p.get_sample_size(FORMAT))
+            final_audio.setsampwidth(aud.get_sample_size(FORMAT))
             final_audio.setframerate(RATE)
-            final_audio.writeframes(b''.join(frames))
+            final_audio.writeframes(b''.join(audio_output))
             final_audio.close()
 
             video_output.release()
@@ -78,7 +140,7 @@ def base_function():
     
     record_if_suspicious(vid_frame,aud_frame)
 
-    imgtk = ImageTk.PhotoImage(image=Image.fromarray(cv2.cvtColor(vid_frame, cv2.COLOR_BGR2RGBA)))
+    imgtk = ImageTk.PhotoImage(image=Image.fromarray(cv2.cvtColor(cv2.resize(vid_frame, (0, 0), fx = 0.5, fy = 0.5) , cv2.COLOR_BGR2RGBA)))
     lmain.imgtk = imgtk
     lmain.configure(image=imgtk)
     lmain.after(1, base_function)   
