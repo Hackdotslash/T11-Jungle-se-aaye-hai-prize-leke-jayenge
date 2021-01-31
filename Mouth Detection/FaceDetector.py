953d7c0b7826357f08ce293104e3c13eb0201727
Deepanshu Raj <dcode711@gmail.com>
2021-01-31 08:28:51 +0530

    Mouth movement tracking
    -added script for facial landmark detection.
    -added script for mouth opening detection.

--- Mouth Detection/FaceDetector.py
+++ Mouth Detection/FaceDetector.py
@@ -1,7 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 29 17:52:00 2020
+
+@author: hp
+"""
+
 import cv2
 import numpy as np
-import tensorflow as tf
-from tensorflow import keras
 
 def get_face_detector(modelFile=None,
                       configFile=None,
@@ -21,6 +26,7 @@ def get_face_detector(modelFile=None,
     Returns
     -------
     model : dnn_Net
+
     """
     if quantized:
         if modelFile == None:
@@ -47,13 +53,16 @@ def find_faces(img, model):
         Image to find faces from
     model : dnn_Net
         Face detection model
+
     Returns
     -------
     faces : list
         List of coordinates of the faces detected in the image
+
     """
     h, w = img.shape[:2]
-    blob = cv2.dnn.blobFromImage(cv2.resize(img, (300, 300)), 1.0,(300, 300), (104.0, 177.0, 123.0))
+    blob = cv2.dnn.blobFromImage(cv2.resize(img, (300, 300)), 1.0,
+	(300, 300), (104.0, 177.0, 123.0))
     model.setInput(blob)
     res = model.forward()
     faces = []
@@ -64,3 +73,23 @@ def find_faces(img, model):
             (x, y, x1, y1) = box.astype("int")
             faces.append([x, y, x1, y1])
     return faces
+
+def draw_faces(img, faces):
+    """
+    Draw faces on image
+
+    Parameters
+    ----------
+    img : np.uint8
+        Image to draw faces on
+    faces : List of face coordinates
+        Coordinates of faces to draw
+
+    Returns
+    -------
+    None.
+
+    """
+    for x, y, x1, y1 in faces:
+        cv2.rectangle(img, (x, y), (x1, y1), (0, 0, 255), 3)
+        
